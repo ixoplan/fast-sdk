762f4e2d622a6a9646df954d9c4fd8323a3d2436
Markus Marchewa <m.marchewa@ixolit.com>
2017-05-10 10:27:00 +0200

    fixed sitemap renderer, urlset attributes

--- src/SitemapRenderer.php
+++ src/SitemapRenderer.php
@@ -33,14 +33,16 @@ class SitemapRenderer implements Interfaces\SitemapRenderer {
 	function render($vhost = null, $languages = []) {
 		$output = '';
 		$output .= '<?xml version="1.0" encoding="UTF-8"?>';
-		$output .= '<urlset xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" ' .
-			'xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9' .
-			' http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"' .
-			'xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 
 		if (!$vhost) {
 			$vhost = $this->requestApi->getVhost();
 		}
+		$output .= '<urlset'.
+			' xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"'.
+			' xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"' .
+			' xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9' .
+				' http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd"' .
+			'>';
 
 		if (empty($languages)) {
 			$languages = $this->pagesApi->getLanguages();
