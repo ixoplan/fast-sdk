f9cf53686be8407ab3db57e281485acf5933f541
Markus Marchewa <m.marchewa@ixolit.com>
2017-06-13 14:08:37 +0200

    phpdoc

--- src/Context/Page.php
+++ src/Context/Page.php
@@ -19,7 +19,7 @@ use Psr\Http\Message\UriInterface;
 /**
  * Context instance to be initialized once, providing utilities related to pages requests
  *
- * @package Ixolit\CDE\View
+ * @package Ixolit\CDE\Context
  */
 class Page {
 
