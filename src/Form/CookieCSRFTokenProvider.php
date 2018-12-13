2af0ff1fb04d7e06a574aee7df9b24de1cc3ef60
Florian Riegler <f.riegler@ixolit.com>
2018-12-13 11:12:18 +0100

    added kvs csrf salt config + fixed provider PHP_INT_MAX is not known within CDE environment

--- src/Form/CookieCSRFTokenProvider.php
+++ src/Form/CookieCSRFTokenProvider.php
@@ -6,6 +6,10 @@ use Ixolit\CDE\CDECookieCache;
 use Ixolit\CDE\Interfaces\RequestAPI;
 use Ixolit\CDE\Interfaces\ResponseAPI;
 
+/**
+ * Class CookieCSRFTokenProvider
+ * @package Ixolit\CDE\Form
+ */
 class CookieCSRFTokenProvider implements CSRFTokenProvider {
 
 	const COOKIE_NAME_CSRF_TOKEN = 'csrf-token';
@@ -14,10 +18,21 @@ class CookieCSRFTokenProvider implements CSRFTokenProvider {
 	 * @var RequestAPI
 	 */
 	private $requestAPI;
+
 	/**
 	 * @var ResponseAPI
 	 */
 	private $responseAPI;
+
+    /**
+     * @var string
+     */
+	private $storedToken;
+
+    /**
+     * @var string
+     */
+	private $nextToken;
 
 	public function __construct(
 		RequestAPI $requestAPI,
@@ -26,16 +41,42 @@ class CookieCSRFTokenProvider implements CSRFTokenProvider {
 		$this->requestAPI = $requestAPI;
 		$this->responseAPI = $responseAPI;
 
-		CDECookieCache::getInstance()->write(self::COOKIE_NAME_CSRF_TOKEN, $this->getCSRFToken());
+		$this->storedToken = $this->readCookieToken();
+		$this->nextToken = $this->generateToken();
+
+		CDECookieCache::getInstance()->write(self::COOKIE_NAME_CSRF_TOKEN, $this->getCSRFToken(), CDECookieCache::COOKIE_TIMEOUT_SESSION, null, null, false, true);
 	}
 
+	private function readCookieToken() {
+	    $token = CDECookieCache::getInstance()->read(self::COOKIE_NAME_CSRF_TOKEN);
+
+        if (!strlen($token)) {
+            //will be invalid, because we regenerate it
+            $token = $this->generateToken();
+        }
+
+        return $token;
+	}
+
+	private function generateToken() {
+	    //we assume 32 bit here on purpose
+	    return \md5(\mt_rand(0, defined('PHP_INT_MAX') ? PHP_INT_MAX : 2147483647));
+	}
+
+    /**
+     * Returns the next csrf token
+     * @return string
+     */
 	public function getCSRFToken() {
-		$token = CDECookieCache::getInstance()->read(self::COOKIE_NAME_CSRF_TOKEN);
-
-		if ($token === null) {
-			$token = \md5(\mt_rand(0, PHP_INT_MAX));
-		}
-
-		return $token;
+	    return $this->nextToken;
 	}
+
+    /**
+     * Returns the previously generated token which got stored as cookie value
+     * @return string
+     */
+	public function getStoredCSRFToken(){
+        return $this->storedToken;
+    }
+
 }
